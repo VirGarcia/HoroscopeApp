825ac136aa4be8269e3bf80a5578952725a63ef0
VirGarcia <vir_garcia_minero@hotmail.com>
2024-06-18 13:00:37 +0200

    paso previo a implantar pedir nombre usuario

--- app/src/main/java/com/example/horoscopeapp/activities/DetailActivity.kt
+++ app/src/main/java/com/example/horoscopeapp/activities/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.horoscopeapp.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageView
